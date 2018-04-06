eb6939da1d1013cbcb68555b3f8735416a5a1cf2
Arnaldo Francisco Vitaliano Filho <arnaldo.vitaliano@bcb.gov.br>
2018-04-06 13:13:52 -0300

    removed docker aliases. not using them.

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -10,8 +10,8 @@ source ~/.zsh.d/functions.zsh
 source ~/.zsh.d/alias.zsh
 source ~/.zsh.d/alias-git.zsh
 source ~/.zsh.d/alias-dir.zsh
-source ~/.zsh.d/alias-docker.zsh
-source ~/.zsh.d/alias-emacs.zsh
+#source ~/.zsh.d/alias-docker.zsh
+#source ~/.zsh.d/alias-emacs.zsh
 source ~/.zsh.d/alias-vim.zsh
 source ~/.zsh.d/prompt.zsh
 source ~/.zsh.d/ninplug.zsh
